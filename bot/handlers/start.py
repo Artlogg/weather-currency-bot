09c10d5b2345433595dc126ea18ceb86c96214a1
zubalov6 <vladimirzubalov06@gmail.com>
2026-01-04 14:12:12 +0300

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from bot.keyboards.main import main_menu, general_menu, weather_menu, rates_menu
+from bot.keyboards.main import general_menu, main_menu, weather_menu, rates_menu
 
 router = Router()
 
