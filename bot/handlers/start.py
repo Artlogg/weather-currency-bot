524d15bb3e048a4ad47a2c86d244de542e2dc0bb
zubalov6 <vladimirzubalov06@gmail.com>
2026-01-04 14:26:12 +0300

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,9 +2,12 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from bot.keyboards.main import main_menu
+
+from bot.keyboards.main import main_menu, general_menu, rates_menu, weather_menu
+
 
 router = Router()
+
 
 @router.message(CommandStart())
 async def start(message: Message) -> None:
@@ -13,5 +16,24 @@ async def start(message: Message) -> None:
         "Команды:\n"
         "/weather <город> — погода по городу\n"
         "/rate <BASE> <TARGET> — курс валют\n\n"
-        "Пример: /weather Riga", reply_markup=main_menu,
+        "Пример: /weather Riga",
+        reply_markup=main_menu,
     )
+
+
+@router.message()
+async def handle_main_buttons(message: Message):
+    text = message.text
+
+    if text == "🌤 Погода":
+        await message.answer("Выберите опцию:", reply_markup=weather_menu)
+    elif text == "💱 Курсы валют":
+        await message.answer("Выберите пару:", reply_markup=rates_menu)
+    elif text == "⚙️ Общее":
+        await message.answer("Настройки:", reply_markup=general_menu)
+    elif text == "⬅️ Назад":
+        await message.answer("Главное меню:", reply_markup=main_menu)
+    elif text == "❌ Убрать клавиатуру":
+        await message.answer("Клавиатура убрана", reply_markup=None)
+    else:
+        await message.answer("Выберите действие из меню!")
