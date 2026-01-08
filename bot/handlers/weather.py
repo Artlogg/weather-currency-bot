2dc46503e30005513c3177363f99bc85eeff5fde
zubalov6 <vladimirzubalov06@gmail.com>
2026-01-08 22:58:32 +0300

    style: fix ruff

--- bot/handlers/weather.py
+++ bot/handlers/weather.py
@@ -6,7 +6,6 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 
 from bot.keyboards.main import weather_menu
-
 from bot.services.weather_client import WeatherClient
 from bot.states.weather import WeatherStates
 
